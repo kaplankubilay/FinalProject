8ff06cfdcff8da099d98da9b0835ea4ab85b0589
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-06-15 21:09:15 +0300

    Controller ve dependencyInjection yapısı kuruldu. Startup a IOC container yapısı eklendi.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -5,6 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 
 namespace WebAPI.Controllers
 {
@@ -12,15 +15,19 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         [HttpGet]
         public List<Product> Get()
         {
-            return new List<Product>
-            {
-                new Product{ ProductId=1,ProductName="elma"},
-                new Product{ ProductId=2,ProductName="saman"},
-                1.ders sonu
-            };
+            var result = _productService.GetAll();
+            return result.Data;
+            //2.ders sonu
         }
     }
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -10,6 +10,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 
 namespace WebAPI
 {
@@ -26,6 +30,9 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //Manager içinde veri tutulmuyorsa singleton yapýlabilir aksi halde örn tüm sepetler ayný olur. Data db de tutuluyorsa yine singleton yapýlabilir.
+            services.AddSingleton<IProductService,ProductManager>();
+            services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
