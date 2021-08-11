4b8c002da543e4cbea952025163f3c86872f37bd
Kubilay Kaplan <d_kubilayk@tofas.com.tr>
2021-08-11 11:15:38 +0300

    fixed.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -43,7 +43,7 @@ namespace WebAPI.Controllers
             var result = _productService.GetByProductId(productId);
             if (result.Success)
             {
-                return Ok(result.Data);
+                return Ok(result);
             }
 
             return BadRequest(result);
